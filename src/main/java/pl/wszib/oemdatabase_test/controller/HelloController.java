28a1d9e1c7d0a8ba10afc1b3cd3681df76845405
Marcin <lmartino@wp.pl>
2023-07-15 14:32:00 +0200

    Prosty kontroller restowy do testów

---
+++ src/main/java/pl/wszib/oemdatabase_test/controller/HelloController.java
@@ -0,0 +1,12 @@
+package pl.wszib.oemdatabase_test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("hello")
+    public String hello() {
+        return "Hello!!!";
+    }
+}
