17a956d67f63accb9688f8f9921a4afeba827c4a
Marcin <lmartino@wp.pl>
2023-08-12 13:36:39 +0200

    Aktualizacja nazwy projektu - refaktoring plików

--- src/main/java/pl/wszib/oemdatabase/OemDataBaseApplication.java
+++ src/main/java/pl/wszib/oemdatabase/OemDataBaseApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OemDataBaseTestApplication {
+public class OemDataBaseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OemDataBaseTestApplication.class, args);
+        SpringApplication.run(OemDataBaseApplication.class, args);
     }
 
 }
